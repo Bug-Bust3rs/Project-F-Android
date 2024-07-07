ee18e0a48328dee0ad82bf225a7b2b9d2d15cc7a
Puskar-Roy <puskarroy600@gmail.com>
2024-07-07 19:33:28 +0530

    feat : all pages

--- app/auth/login/index.jsx
+++ app/auth/login/index.jsx
@@ -47,7 +47,7 @@ export default function login() {
               <Text className='font-medium text-4xl text-cyan-400'>User !</Text>
 
             </View>
-            <Text className='text-gray-500'>Sign up or Login to your account.</Text>
+            <Text className='text-gray-500 font-light'>Sign up or Login to your account.</Text>
 
           </View>
           <View className='bg-slate-200 p-3 rounded-2xl flex flex-row '>
@@ -62,37 +62,55 @@ export default function login() {
         </View>
 
 
-   
+
 
         {
           toogle &&
-          <View>
+          <View className=''>
+            <View className=''>
 
-            <View className='flex gap-5'>
-              <View>
+              <View className='flex gap-5 mb-3'>
+                <View>
 
-                <Text className='text-base font-normal mb-1 ml-2'>Email Address</Text>
-                <TextInput
-                  className='px-3 py-3 bg-slate-200 text-base rounded-2xl'
-                  placeholder="Email"
-                  value={email}
-                  onChangeText={setEmail}
-                />
+                  <Text className='text-base font-normal mb-1 ml-2'>Email Address</Text>
+                  <TextInput
+                    className='px-3 py-3 bg-slate-200 text-base rounded-2xl'
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                  />
+                </View>
+                <View>
+                  <Text className='text-base font-normal mb-1 ml-2'>Password</Text>
+                  <TextInput
+                    className='px-3 py-3 bg-slate-200 text-base rounded-2xl'
+                    placeholder="Password"
+                    value={email}
+                    onChangeText={setEmail}
+                  />
+                </View>
               </View>
-              <View>
-                <Text className='text-base font-normal mb-1 ml-2'>Password</Text>
-                <TextInput
-                  className='px-3 py-3 bg-slate-200 text-base rounded-2xl'
-                  placeholder="Password"
-                  value={email}
-                  onChangeText={setEmail}
-                />
+              <View className='mb-5 flex items-end justify-center'>
+                <Text className='text-end font-light text-slate-500'>Forgot Password?</Text>
               </View>
+              <View className='flex flex-row w-full justify-center items-center gap-2'>
+
+                  <Text className='text-base font-light text-slate-700'>Or Login Using</Text>
+                <Text className='bg-slate-700 h-[2px] rounded-xl flex-1'>Or Login Using</Text>
+
+
+
+              </View >
+                <View className='flex flex-row gap-5 my-5'>
+                  <Image className='h-12 w-12 shadow-lg' source={require('../../../assets/images/google.png')} />
+                  <Image className='h-12 w-12 shadow-lg' source={require('../../../assets/images/githubl.png')} />
+                  <Image className='h-12 w-12 shadow-lg' source={require('../../../assets/images/fb.png')} />
+                </View>
+              <TouchableOpacity className='bg-cyan-400 mt-5 px-2 py-3 rounded-2xl hover:bg-rose-400 cursor-pointer mb-[20px]'>
+                <Text className='text-center text-white text-base font-semibold'>Lets Go!</Text>
+              </TouchableOpacity>
             </View>
-
-            <TouchableOpacity className='bg-cyan-400 px-2 py-3 rounded-2xl hover:bg-rose-400 cursor-pointer mb-[20px]'>
-              <Text className='text-center font-bold text-white text-lg'>Register</Text>
-            </TouchableOpacity>
+            
           </View>
         }
         {
@@ -102,7 +120,25 @@ export default function login() {
             <View className='flex gap-5'>
               <View>
 
-                <Text className='text-base font-normal mb-1 ml-2'>Email Address</Text>
+                <Text className='text-base font-normal mb-1 ml-2'>Name</Text>
+                <TextInput
+                  className='px-3 py-3 bg-slate-200 text-base rounded-2xl'
+                  placeholder="Name"
+                  value={email}
+                  onChangeText={setEmail}
+                />
+              </View>
+              <View>
+                <Text className='text-base font-normal mb-1 ml-2'>Phone No</Text>
+                <TextInput
+                  className='px-3 py-3 bg-slate-200 text-base rounded-2xl'
+                  placeholder="Phone No"
+                  value={email}
+                  onChangeText={setEmail}
+                />
+              </View>
+              <View>
+                <Text className='text-base font-normal mb-1 ml-2'>Email</Text>
                 <TextInput
                   className='px-3 py-3 bg-slate-200 text-base rounded-2xl'
                   placeholder="Email"
@@ -121,7 +157,7 @@ export default function login() {
               </View>
             </View>
 
-            <TouchableOpacity className='bg-cyan-400 px-2 py-3 rounded-2xl hover:bg-rose-400 cursor-pointer mb-[20px]'>
+            <TouchableOpacity className='bg-cyan-400 mt-7 px-2 py-3 rounded-2xl hover:bg-rose-400 cursor-pointer mb-[20px]'>
               <Text className='text-center font-bold text-white text-lg'>Register</Text>
             </TouchableOpacity>
           </View>
